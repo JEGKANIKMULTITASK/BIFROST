98895a7d859821facf9f0d24eb1bac2a020e8f21
KristineKrighaar <61507607+KristineKrighaar@users.noreply.github.com>
2024-04-03 13:05:22 +0200

    Phonon in multible slices

--- McStasScript/run_folder/Run_more_settings.sh
+++ McStasScript/run_folder/Run_more_settings.sh
@@ -2,12 +2,11 @@
 
 for i in $(seq 0 1 90)
 do
-    mcrun -c --mpi=6 backend_Pb_3t.instr -d Pb_290K_70_$i -n 1000000  A4=70  A3=$i
+    mcrun -c --mpi=6 backend_Pb_3t.instr -d Pb_290K_20_$i -n 1000000  A4=20  A3=$i
 done
-
 
 
 for i in $(seq 0 1 90)
 do
-    mcrun -c --mpi=6 backend_Pb_3t.instr -d Pb_290K_75_$i -n 1000000  A4=75  A3=$i
+    mcrun -c --mpi=6 backend_Pb_3t.instr -d Pb_290K_15_$i -n 1000000  A4=15  A3=$i
 done
